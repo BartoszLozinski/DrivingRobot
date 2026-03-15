83ddee2a2d1e30c6b107fbc6d71eb836eb69417a
Bartosz Lozinski <lozinski.bart@gmail.com>
2026-03-15 13:26:00 +0100

    Added Flash script

---
+++ Flash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+openocd -f /usr/share/openocd/scripts/interface/stlink.cfg \
+        -f /usr/share/openocd/scripts/target/stm32l4x.cfg \
+        -c "program build/Core/DrivingRobot verify reset exit"
