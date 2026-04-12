0fec43ab1ebbd72f6015f66eecdd604f8f8c6855
Bartosz Lozinski <lozinski.bart@gmail.com>
2026-04-12 18:20:36 +0200

    Removed adc temp sensor, for code simplification

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -4,7 +4,6 @@
 #include <cstring>
 #include <string_view>
 
-#include "../../Peripherals/ADC/HAL/Adc.hpp"
 #include "../../Peripherals/Timer/HAL/SoftwareTimer.hpp"
 #include "../../Peripherals/Timer/HAL/InputCapture.hpp"
 #include "../../Peripherals/Timer/HAL/Pwm.hpp"
@@ -14,7 +13,6 @@
 #include "../../Devices/HC_SR04.hpp"
 #include "../../Devices/LM35.hpp"
 
-ADC_HandleTypeDef hadc1;
 TIM_HandleTypeDef htim2;
 UART_HandleTypeDef huart2;
 TIM_HandleTypeDef htim3;
@@ -24,7 +22,6 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_TIM2_Init(void);
-static void MX_ADC1_Init(void);
 static void MX_TIM3_Init(void);
 
 int main()
@@ -37,21 +34,15 @@ int main()
     MX_GPIO_Init();
     MX_USART2_UART_Init();
     MX_TIM2_Init();
-    MX_ADC1_Init();
     MX_TIM3_Init();
 
-
-
     
-    Peripherals::HAL::Adc adc1{ hadc1 };
     Peripherals::HAL::InputCapture timer2Channel1Rising{ htim2, TIM_CHANNEL_1 }; //PA0
     Peripherals::HAL::InputCapture timer2Channel2Falling{ htim2, TIM_CHANNEL_2 }; //PA0
     Peripherals::HAL::Pwm distanceMeasurementTrigger{ htim3, TIM_CHANNEL_1 }; //PC6
     Peripherals::HAL::Uart uart2{ huart2 };
 
-    float temp = 0.0f;
     float distance = 0.0f;
-    uint32_t adcValue = 0;
     char stringBuffer[64];
     HAL::SoftwareTimer printingTimer{ 500 };
 
@@ -59,7 +50,6 @@ int main()
                            , timer2Channel2Falling
                            , distanceMeasurementTrigger
                            , HAL::SoftwareTimer{ 50 }};
-    Device::LM35 lm35{ adc1 };
 
     HAL::SoftwareTimer uartResetTimer{ 2000 };
     UcCommunication::LineParser<Peripherals::HAL::Uart<64>> lineParser{ uart2 };
@@ -92,11 +82,8 @@ int main()
             uart2.Transmit(reinterpret_cast<const uint8_t*>(resetMsg.data()), resetMsg.size());
         }
         // End of UART Test
-
-        adcValue = adc1.Read();
-        temp = lm35.ReadTempC();
         
-        if (const auto distanceOpt = hc_sr04.GetDistance(temp))
+        if (const auto distanceOpt = hc_sr04.GetDistance())
             distance = distanceOpt.value();
 
         if (printingTimer.IsExpired())
@@ -104,12 +91,9 @@ int main()
             printingTimer.Reset();
             snprintf(stringBuffer, sizeof(stringBuffer), "Distance: %.1f [cm]\r\n", distance);
             uart2.Transmit(reinterpret_cast<const uint8_t*>(stringBuffer), strlen(stringBuffer));
-            snprintf(stringBuffer, sizeof(stringBuffer), "ADC: %lu[-], Temp: %.1f [C]\r\n", adcValue, temp);
-            uart2.Transmit(reinterpret_cast<const uint8_t*>(stringBuffer), strlen(stringBuffer));
         }
     }
 }
-
 
 
 //Stm32CubeIDE generated functions
@@ -146,49 +130,6 @@ void SystemClock_Config(void)
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-    {
-        Error_Handler();
-    }
-}
-
-static void MX_ADC1_Init(void)
-{
-    ADC_MultiModeTypeDef multimode = {};
-    ADC_ChannelConfTypeDef sConfig = {};    /** Common config
-    */
-    hadc1.Instance = ADC1;
-    hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
-    hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-    hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-    hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-    hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-    hadc1.Init.LowPowerAutoWait = DISABLE;
-    hadc1.Init.ContinuousConvMode = ENABLE;
-    hadc1.Init.NbrOfConversion = 1;
-    hadc1.Init.DiscontinuousConvMode = DISABLE;
-    hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-    hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-    hadc1.Init.DMAContinuousRequests = DISABLE;
-    hadc1.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
-    hadc1.Init.OversamplingMode = DISABLE;
-    if (HAL_ADC_Init(&hadc1) != HAL_OK)
-    {
-        Error_Handler();
-    }   /** Configure the ADC multi-mode
-    */
-    multimode.Mode = ADC_MODE_INDEPENDENT;
-    if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
-    {
-        Error_Handler();
-    }   /** Configure Regular Channel
-    */
-    sConfig.Channel = ADC_CHANNEL_11;
-    sConfig.Rank = ADC_REGULAR_RANK_1;
-    sConfig.SamplingTime = ADC_SAMPLETIME_640CYCLES_5;
-    sConfig.SingleDiff = ADC_SINGLE_ENDED;
-    sConfig.OffsetNumber = ADC_OFFSET_NONE;
-    sConfig.Offset = 0;
-    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
     {
         Error_Handler();
     }
--- Peripherals/ADC/HAL/Adc.hpp
+++ Peripherals/ADC/HAL/Adc.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-
 #include "AdcBase.hpp"
 
 extern "C"
